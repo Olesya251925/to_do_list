31d58c60fc62355c982ca1988acd066e84f7f8cb
Olesya <olesya.kiril2014@gmail.com>
2024-10-04 22:57:41 +0700

    add cursor to tasks

--- src/data/storage.js
+++ src/data/storage.js
@@ -1,9 +1,7 @@
-// Сохранение задач в localStorage
 function saveTasks(tasks) {
     localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
-// Загрузка задач из localStorage
 function loadTasks() {
     const tasks = localStorage.getItem('tasks');
     return tasks ? JSON.parse(tasks) : [];
